b850fdc4f8aee6f926759544dd2c7cab3a09c4f6
arusmanukyan <manukyan.arus@gmail.com>
2016-12-03 20:38:25 -0800

    hw11

--- liri.js
+++ liri.js
@@ -1,41 +1,91 @@
 
-// var myKeys = require("./keys.js");
+var myKeys = require("./keys.js");
+var twitter = require("twitter");
+var client = new twitter(myKeys.twitterKeys);
+var spotify = require("spotify"); 
+var request = require("request");
 
-// var twitter = require("twitter");
- 
-// var client = new twitter(myKeys.twitterKeys);
- 
-// var params = {screen_name: 'arusmanukyan', count: 20};
-// client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//   if (error) {
-//   	console.log("error" + error)
-//   }else{
-//   	for (var i = 0; i < tweets.length; i++) {
-  	
-//     console.log(tweets[i].text);
-// 	}
-//   }
-// });
 
-var spotify = require('spotify');
+ var command = process.argv[2];
+ if (command =="my-tweets"){
+ 	myTweets()
+ };
+ if (command =="spotify-this-song"){
+ 	mySpotify()
+ };
+ if (command =="movie-this"){
+ 	movie()
+ };
 
-var first = process.argv[2];
-var second = process.argv[3];
- 
-spotify.search({ type: 'track', query: '' }, function(error, data) {
+
+
+function myTweets(){
+var params = {screen_name: 'arusmanukyan', count: 20};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (error) {
+  	console.log("error" + error)
+  }else{
+  	for (var i = 0; i < tweets.length; i++) {
+  	console.log("---------------------------")
+    console.log(tweets[i].text);
+	console.log("---------------------------")
+	}
+  }
+});
+}
+
+
+function mySpotify(){
+var name = [];
+var userFirst = process.argv[2];
+var userSecond = process.argv[3];
+
+spotify.search({ type: 'track', query: "name" }, function(error, response) {
     if (error) {
         console.log('Error occurred: ' + error);
+        return;
     }else{
-    	for (var i = 0; i < data.length; i++) {
-        console.log(data[i]);
+    	
+        console.log("Artist: " + response.tracks.items[0].artist);
+        console.log("");
+        console.log("Song name: " + response.tracks.items[0].name);
+        console.log("")
+        console.log("Link: " + response.tracks.items[0].preview_url);
+        console.log("")
+        console.log("Album: " + response.tracks.items[0].album);
       }
- }
+ })
  
-});
+};
 
-// var request = require('request');
-// request('http://www.google.com', function (error, response, body) {
-//   if (!error && response.statusCode == 200) {
-//     console.log(body) // Show the HTML for the Google homepage. 
-//   }
-// })
+ function movie(){
+  
+  var movieName = "frozen";
+  
+  var queryURL = "http://omdbapi.com/?t=" + movieName + "&tomatoes=true" + "&r=json";
+
+request(queryURL, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var result = JSON.parse(body);
+        console.log("Title: " + result.Title);
+        console.log("");
+        console.log("Year: " + result.Year);
+        console.log("");
+        console.log("IMDB Rating: " + result.imdbRating);
+        console.log("");
+        console.log("Country of Production: " + result.Country);
+        console.log("");
+        console.log("Language: " + result.Language);
+        console.log("");
+        console.log("Plot Summary: " + result.Plot);
+        console.log("");
+        console.log("Actors: " + result.Actors);
+        console.log("");
+        console.log("Rotten Tomatoes Rating: " + result.tomatoRating);
+        console.log("");
+        console.log("Rotten Tomatoes URL: " + result.tomatoURL);
+        console.log("");
+
+  }
+})
+};
